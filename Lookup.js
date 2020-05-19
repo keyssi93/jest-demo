26ff3b377e98e9e1fe41cad0b311b529342880e8
mrdoomus <mrdoom.official@gmail.com>
2020-05-18 21:24:42 -0500

    demo: [Update] uplodaded code for workshop

--- Lookup.js
+++ Lookup.js
@@ -1,1 +1,40 @@
 //Lookup.js
+
+// Data
+const users = [
+    {
+      userName: 'doom',
+      firstName: 'Camilo',
+      lastName: 'Villegas',
+      age: 22,
+      likes: ['Javascript', 'Python', 'C'],
+    },
+    {
+      userName: 'aniki',
+      firstName: 'Billy',
+      lastName: 'Herrington',
+      age: 50,
+      likes: ['C#', 'Ruby', 'Go', 'Kotlin'],
+    },
+    {
+      userName: 'kovrov',
+      firstName: 'Abdel',
+      lastName: 'Osman',
+      age: 99,
+      likes: ['Assembly', 'Cobol'],
+    },
+  ];
+  
+// lookup()
+const lookup = (username, property) => {
+  const user = users.find(u => u.userName === username);
+  if (!user) {
+    throw new Error("Could not find user");
+  }
+  if (!user.hasOwnProperty(property)) {
+    throw new Error("Could not find property");
+  }
+  return user[property];
+};
+
+module.exports = lookup;
--- __tests__/Lookup.test.js
+++ __tests__/Lookup.test.js
@@ -1,1 +1,49 @@
 //Lookup.test.js
+const lookup = require("../Lookup");
+
+/* 
+Write a Suite with the following specs
+  1. Test 'likes' for the specified user
+  2. Test 'age' to be less than 100 for the specified user
+  3. Test error message for unknown user.
+*/
+
+// YOU CODE GOES HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+describe('Testing lookup func', () => {
+    test("lookup(username, property) should return likes for the specified user.", () => {
+      const actual = lookup('doom', 'likes');
+      const expected = ['Javascript', 'Python', 'C'];
+  
+      expect(actual).toEqual(expected);
+    });
+
+    test("lookup(username, property) should return the age for the specified user", () => {
+      const actual = lookup('kovrov', 'age');
+  
+      expect(actual).toBeLessThan(100);
+    });
+
+    it("lookup(username, property) with unknown user should throw an error with the correct message", () => {
+      expect(() => {
+        lookup('nobody', 'likes');
+      }).toThrowError("Could not find user");
+    });
+
+  });
+*/
